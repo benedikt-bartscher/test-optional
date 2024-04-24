214ba97da3e7131a16af99d2f7a1fa0caec2d37e
Benedikt Bartscher <bb@senbax.de>
2024-04-24 21:34:39 +0200

    extend example

--- test_optional/test_optional.py
+++ test_optional/test_optional.py
@@ -3,12 +3,26 @@ import reflex as rx
 
 
 class State(rx.State):
-    opt: str | None = "bla"
-    opt_2: Optional[str] = None
+    no_default: str
+    str_default: str = "bla"
+    opt_no_default: Optional[str]
+    opt_str_default: Optional[str] = "bla"
+    opt_none_default: Optional[str] = None
 
 
 def index() -> rx.Component:
-    return rx.input(value=State.opt)
+    return rx.vstack(
+        # works fine
+        rx.input(value=State.no_default),
+        # works fine
+        rx.input(value=State.str_default),
+        # TypeError: Invalid var passed for prop value, expected type typing.Union[str, int, bool], got value opt_no_default of type typing.Optional[str].
+        # rx.input(value=State.opt_no_default),
+        # works fine
+        rx.input(value=State.opt_str_default),
+        # TypeError: Invalid var passed for prop value, expected type typing.Union[str, int, bool], got value opt_none_default of type typing.Optional[str].
+        # rx.input(value=State.opt_none_default),
+    )
 
 
 app = rx.App()
